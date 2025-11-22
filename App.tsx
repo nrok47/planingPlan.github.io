314f44fe973e5acc001a646c331aefffffb00907
nrok47 <107177125+nrok47@users.noreply.github.com>
2025-11-22 07:29:44 +0000

    before

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { ProjectGanttChart } from './components/ProjectGanttChart';
 import { ProjectModal } from './components/ProjectModal';
